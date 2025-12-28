9d374ba71767630a1915b93d2621239cddfa8044
andresaguilar1125 <andresaguilar1125@gmail.com>
2025-12-27 20:21:52 -0600

    sidebar

--- app.py
+++ app.py
@@ -1,12 +1,30 @@
-"""
-Minimal Streamlit expense tracker demo for the `gastos` project.
-Run: pip install -r requirements.txt
-     streamlit run streamlit_app.py
-"""
-
 import streamlit as st
 import pandas as pd
 from datetime import date
 from data.gsheets import transform_gsheet, load_gsheet
 
-st.write(transform_gsheet(load_gsheet()))
+st.set_page_config(page_title="GSheet Data Viewer", layout="wide")
+
+df = transform_gsheet(load_gsheet())
+
+with st.sidebar:
+    st.title("Gastos")
+
+    _periodo = st.selectbox(
+        "Periodo",
+     #    options=df["Periodo"].unique().tolist()
+     options=[0,1,2]
+    )
+
+    _categoria = st.selectbox(
+        "Categoria",
+        options=df["Categoria"].unique().tolist()
+    )
+
+    _persona = st.selectbox(
+        "Persona",
+        options=df["Persona"].unique().tolist()
+    )
+
+st.write(df)
+
